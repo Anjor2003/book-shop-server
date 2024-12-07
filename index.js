715d7001179f23827688c51335908d2d1faae5e7
Anjor2003 <101336015+Anjor2003@users.noreply.github.com>
2024-12-07 12:46:31 +0100

    Update cors

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
+     "Access-Control-Allow-Origin": "*",
     origin: [
       "http://localhost:5173",
       "https://book-shop-frontend-indol.vercel.app",
